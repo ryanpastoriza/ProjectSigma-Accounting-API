f94f740c6c130a4d3a0f5fac405f8e0cf437561e
ryanpastoriza <ryanpastoriza@gmail.com>
2024-10-17 09:06:23 +0800

    update

--- app/Http/Resources/AccountsResource.php
+++ app/Http/Resources/AccountsResource.php
@@ -15,6 +15,7 @@ class AccountsResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+			'id' => $this->id,
 			'account_type_id' => $this->account_type_id,
 			'account_number' => $this->account_number,
 			'account_name' => $this->account_name,
